137f02d0c9c2322f69e3f87cad03c700bfad7e92
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2024-10-08 12:56:20 -0500

    :hammer: Examples in progress

---
+++ POO - Encapsulamiento/script2.js
@@ -0,0 +1,15 @@
+const edadSimbolo = Symbol("edad");
+class Persona {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.edadSimbolo = 25;
+    }
+
+    getEdad() {
+        let Years = `Edad: `+Number(this.edadSimbolo);
+        return Years;
+    }
+}
+
+const maria = new Persona("Maria");
+console.log(maria.getEdad());
